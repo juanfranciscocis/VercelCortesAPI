efdc4f2fdd9fd517fb98ec421e2b0431bfddffe7
Juan Francisco Cisneros <jfcisnerosg@icloud.com>
2024-11-17 22:02:03 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 import google.generativeai as genai
 import os
+from flask import Flask, request, jsonify, send_file
+import urllib3
+from pypdf import PdfReader
 import tempfile
-from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
-import urllib3
 
 # Configuración de Flask
 app = Flask(__name__)
@@ -16,49 +17,18 @@ genai.configure(api_key="AIzaSyCJ1biYiA_IErtPWpUXMX7pPNeFWj3h_RM")
 @app.route("/api", methods=["GET"])
 def get_power_outage():
     zona = request.args.get("zona")
-    if not zona:
-        return jsonify({"error": "Zona es un parámetro obligatorio"}), 400
 
-    try:
-        # Descargar el PDF en memoria
-        pdf_content, error = scrapear_y_obtener_pdf(
-            "https://www.eeq.com.ec/cortes-de-servicio1"
-        )
-        if error:
-            return jsonify({"error": error}), 400
+    data = informacion_de_zona(zona)
 
-        # Escribir el PDF en un archivo temporal
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_pdf:
-            temp_pdf.write(pdf_content)
-            temp_pdf_path = temp_pdf.name
+    jsonfinal = {
+        "zona": zona,
+        "horarios": data
+    }
 
-        # Subir el archivo PDF a Gemini
-        file = genai.upload_file(path=temp_pdf_path, display_name="Corte Luz UIO")
+    return jsonify({"data": jsonfinal})
 
-        # Usar modelo generativo
-        model = genai.GenerativeModel(model_name="models/gemini-1.5-flash")
 
-        # Obtener la fecha actual
-        date = os.popen("date +%Y-%m-%d").read().strip()
-        prompt = (
-            f"Dime el corte de luz para el día {date}, para {zona}, responde de la siguiente manera: "
-            f"en formato json con ciudad, fecha y horarios[]"
-        )
-
-        # Generar contenido
-        response = model.generate_content([prompt, file])
-
-        # Parsear JSON de la respuesta
-        response_text = response.text
-        json_data = response_text.split("```json")[1].split("```")[0].strip()
-
-        # Eliminar el archivo temporal
-        os.remove(temp_pdf_path)
-
-        return jsonify({"data": json_data})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    
 
 
 def scrapear_y_obtener_pdf(url):
@@ -80,7 +50,7 @@ def scrapear_y_obtener_pdf(url):
             # Obtener el enlace (href)
             link = button.get("href")
 
-            # Descargar el contenido del enlace en memoria
+            # Descargar el contenido del enlace
             response = requests.get(link, verify=False)
             if response.status_code == 200:
                 return response.content, None
@@ -90,6 +60,65 @@ def scrapear_y_obtener_pdf(url):
             return None, 'No se encontró el botón con la clase "ver_noticia_home".'
     else:
         return None, f"Error al acceder a la página: {response.status_code}"
+
+def informacion_de_zona(zona):
+    print(f"Zona: {zona}")
+    # Crear un archivo temporal para el PDF descargado
+    pdf_content, error = scrapear_y_obtener_pdf('https://www.eeq.com.ec/cortes-de-servicio1')
+    if error:
+        print(f"Error: {error}")
+        return
+
+    # Guardar el contenido PDF en un archivo temporal
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_pdf:
+        temp_pdf.write(pdf_content)
+        temp_pdf_path = temp_pdf.name
+
+    # Leer el PDF usando PdfReader
+    reader = PdfReader(temp_pdf_path)
+
+    # Imprimir el número de páginas del PDF
+    print(f"Número de páginas: {len(reader.pages)}")
+
+
+
+    # Obtener el contenido pagina por pagina hasta encontrar la zona
+    for i in range(0,len(reader.pages)):
+        page = reader.pages[i]
+        text = page.extract_text()
+
+        # Reemplazar los caracteres especiales
+        text = text.replace("á", "a")
+        text = text.replace("é", "e")
+        text = text.replace("í", "i")
+        text = text.replace("ó", "o")
+        text = text.replace("ú", "u")
+        text = text.replace("ñ", "n")
+        text = text.replace("Á", "A")
+        text = text.replace("É", "E")
+        text = text.replace("Í", "I")
+        text = text.replace("Ó", "O")
+        text = text.replace("Ú", "U")
+        text = text.replace("Ñ", "N")
+
+
+        print(f"Página {page}: {text}")
+
+
+        if zona.upper() in text.upper():
+            # Eliminar el archivo temporal
+            os.remove(temp_pdf_path)
+            # Buscar un / en el texto y extraer el hoario inicial y final 08:00 -12:00 / 4:00 - 8:00
+            index = text.find("/")
+            print(f"Index: {index}")
+            inicio = text[index-13:index-1]
+            fin = text[index+2:index+15]
+            print(f"Inicio: {inicio}")
+            print(f"Fin: {fin}")
+            return inicio, fin
+    
+    os.remove(temp_pdf_path)
+    return "Zona no encontrada"
 
 
 # Punto de entrada
